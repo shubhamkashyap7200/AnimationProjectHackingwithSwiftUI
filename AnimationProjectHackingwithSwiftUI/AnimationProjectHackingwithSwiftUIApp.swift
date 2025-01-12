581cb5c8c8d16a69b8e57606a2d2ef6b7b00f01a
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-12 18:09:25 +0530

    Initial Commit

---
+++ AnimationProjectHackingwithSwiftUI/AnimationProjectHackingwithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationProjectHackingwithSwiftUIApp.swift
+//  AnimationProjectHackingwithSwiftUI
+//
+//  Created by Shubham on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationProjectHackingwithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
